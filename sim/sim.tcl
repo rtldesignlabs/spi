1cf253708542b7c1060b96b90f5be40b8526b624
Isaac Verdu <isaac.verdu@gmail.com>
2020-10-13 05:27:59 +0200

    - Removed the suffix 'top' from the file names - Updated the simulation script and project file with the new file names

--- sim/sim.tcl
+++ sim/sim.tcl
@@ -1,4 +1,4 @@
-exec xvlog  -sv ../hdl/spi_master_top.sv
-exec xvlog  -sv ./spi_master_top_tb.sv
+exec xvlog  -sv ../hdl/spi_master.sv
+exec xvlog  -sv ./spi_master_tb.sv
 exec xelab  spi_master_tb -s spi_master_tb_sim -debug typical
 exec xsim   spi_master_tb_sim -gui
